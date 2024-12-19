53a38f664ecf9806e658ce0ab17969b9c8c9fe2d
casjay <git-admin@casjaysdev.pro>
2024-12-19 11:43:48 -0500

    🗃️ Committing everything that changed 🗃️
    index.js

--- index.js
+++ index.js
@@ -114,6 +114,7 @@ app.post(
 );
 
 app.use((req, res, next) => {
+  const getAgent = req.headers['user-agent'];
   if (getAgent.includes('curl')) {
     setAgent = 'consoleClient';
   } else if (getAgent.includes('wget')) {
@@ -129,6 +130,7 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+  const getAgent = req.headers['user-agent'];
   if (getAgent.includes('curl')) {
     setAgent = 'consoleClient';
   } else if (getAgent.includes('wget')) {
