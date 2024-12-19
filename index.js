e025ad1fb4141e0569324c9ca488b44503805108
casjay <git-admin@casjaysdev.pro>
2024-12-19 12:13:05 -0500

    🗃️ Committing everything that changed 🗃️
    index.js

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ app.use((error, req, res, next) => {
     res.status(500);
   }
   if (setAgent) {
-    const message = JSON.stringify({ message: error.message });
+    const message = error.message;
     res.send(message + '\n');
   } else {
     res.json({
