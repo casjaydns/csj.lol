4d619398567d0de790498d9fb3222e81bc476706
casjay <git-admin@casjaysdev.pro>
2024-12-19 10:45:10 -0500

    🗃️ Committing everything that changed 🗃️
    index.js

--- index.js
+++ index.js
@@ -124,16 +124,16 @@ app.post(
 app.use((req, res, next) => {
   res.status(404).sendFile(notFoundPath);
 });
-
+const error_message = error_message || error.message;
 app.use((error, req, res, next) => {
   if (error.status) {
     res.status(error.status);
   } else {
     res.status(500);
   }
-  console.log(error_message || error.message);
+  console.log(error_message);
   res.json({
-    message: error_message || error.message,
+    message: error_message,
     stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
   });
 });
